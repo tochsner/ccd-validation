f442b7f5b09726978796809bc5906252be1651f7
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-11-22 16:15:21 +0100

    Add last changes.

--- src/datasets/create_yule_datasets.sh
+++ src/datasets/create_yule_datasets.sh
@@ -9,7 +9,7 @@ export LPHY_LIB="/nesi/nobackup/nesi00390/tobia/beast/lphy/lib"
 export BEAST="/nesi/nobackup/nesi00390/tobia/beast/beast"
 
 for n in 10 50 100 200 400; do
-    sh "$BEAST_PKG/lphybeast/bin/lphybeast" ../src/datasets/yule-$n.lphy \
+    sh "$BEAST_PKG/lphybeast/bin/lphybeast" ../src/datasets/yule-$n.lphy \§
         -o ../../data/lphy/yule-$n.xml \
         -r $NUM_REPS \
         -l $CHAIN_LENGTH \
